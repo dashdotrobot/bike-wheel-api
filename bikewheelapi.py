4018108cced18c9ead84bcb90b78245c4827fbb5
Matt Ford <mateoford@gmail.com>
2018-11-27 08:11:09 -0800

    Add skeletons for API endpoints

--- bikewheelapi.py
+++ bikewheelapi.py
@@ -1,7 +1,22 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
+# --------------------------------- ROUTES --------------------------------- #
+# Define application endpointsn                                              #
+# -------------------------------------------------------------------------- #
 @app.route('/')
 def hello():
 	return 'Hello World'
+
+@app.route('/deform')
+def get_deformation():
+	'Return the deformation of a wheel under a set of loads'
+	pass
+
+@app.route('/tensions')
+def get_tensions():
+	'Return the spoke tensions of a wheel under a set of loads'
+	pass
