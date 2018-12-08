a72a763a157518d40c55119567556bafbf2f4871
Matt Ford <mateoford@gmail.com>
2018-12-07 19:47:07 -0600

    Remove scipy and mkl from build

--- bikewheelcalc/helpers.py
+++ bikewheelcalc/helpers.py
@@ -1,7 +1,6 @@
 'Utility functions for bikewheelcalc'
 
 import numpy as np
-from scipy import interpolate
 
 
 def skew_symm(v):
@@ -10,18 +9,6 @@ def skew_symm(v):
     return np.matrix([[0,     v[2], -v[1]],
                       [-v[2], 0,     v[0]],
                       [v[1], -v[0],  0]])
-
-
-def interp_periodic(x, y, xx, period=2*np.pi):
-    'Interpolate a periodic function with cubic spline matching slope at ends.'
-
-    # Pad data by wrapping beginning and end to match derivatives
-    x_pad = np.concatenate(([x[-1] - period], x, [x[0] + period, x[1] + period]))
-    y_pad = np.concatenate(([y[-1]], y, [y[0], y[1]]))
-
-    f_spline = interpolate.splrep(x_pad, y_pad)
-
-    return interpolate.splev(xx, f_spline)
 
 
 def pol2rect(p):
